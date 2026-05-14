08f49cf7935f1830ff415fb390b9357cbb71ddac
Dieter Kybelksties <github@kybelksties.com>
2026-05-14 12:54:41 +0100

    auto-committed by release-to-pypi.py

--- pyjsonobject/types.py
+++ pyjsonobject/types.py
@@ -1,11 +1,9 @@
-from __future__ import annotations
-
 from collections.abc import Iterable
-from typing import TypeAlias
+from typing import Optional, Union
 
-JSONScalar: TypeAlias = bool | int | float | str
-JSONValue: TypeAlias = JSONScalar | list | dict
-JSONObjectContainer: TypeAlias = list | dict
+JSONScalar = Union[bool, int, float, str]
+JSONValue = Union[JSONScalar, list, dict]
+JSONObjectContainer = Union[list, dict]
 
 # mirrors fundamentals.Strings semantics used throughout project
-Strings: TypeAlias = str | Iterable[str] | None
+Strings = Optional[Union[str, Iterable[str]]]
